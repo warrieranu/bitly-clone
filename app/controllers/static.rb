f15f8fc5a34f7be6f09a4bedfa1f29301446186c
Anu Warrier <anuwarrier@ymail.com>
2017-09-08 03:07:21 +0000

    working table

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,27 @@
+require_relative '../../app/models/url.rb'
+
 get '/' do
   erb :"static/index"
+end
+
+post '/url_changer' do
+    params[:long_url]
+    if Url.exists?(long_url: params[:long_url])
+    @p = Url.find_by(long_url: params[:long_url])
+    else
+    @p = Url.new
+    @p.long_url = params[:long_url]
+    @p.short_url = @p.shorten
+    @p.save
+    end
+    # return p.short_url
+    erb :"static/index"
+end
+
+get '/:short_url' do
+    params[:short_url]
+    @p=Url.find_by(short_url: params[:short_url])
+    @p.increment_click_count
+    @p.save
+    redirect to "#{@p.long_url}"
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,24 @@
 class Url < ActiveRecord::Base
+    
+    validates :long_url, presence: true,
+                       format: { with: /[https?]?.*/,
+                                 message: "Not a valid URL"}
+    validates :short_url, uniqueness: true
 
-
-
-
+    after_initialize :set_click_count
+    
+    def set_click_count
+        self.click_count ||= 0
+    end
+    
+    # after_initialize :add_to_table
+    
+    def shorten
+        return "woof-" + SecureRandom.urlsafe_base64(5)
+    end
+ 
+    def increment_click_count
+        self.click_count += 1
+    end
+    
 end
